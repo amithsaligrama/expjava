586026e9006283fb1323d6369a32262d73f6848b
Amith Saligrama <37519861+amithsaligrama@users.noreply.github.com>
2019-03-21 11:46:51 -0400

    Add files via upload

---
+++ PygLatin.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.io.*;
+public class PygLatin {
+
+	public static void main(String[] args) {
+		String first = "a";
+		String word = "b";
+		String rest = "c";
+		String pyg = "ay";
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a string to translate into pyglatin: ");
+		while (true) {
+			try {
+				word = in.nextLine();
+				word = word.toLowerCase();
+				first = word.substring(0,1);
+				rest = word.substring(1);
+				if (rest.substring(0,1).equals(" "))
+					rest = word.substring(2);
+				break;
+			}
+			catch (Exception e) {
+				System.out.println("You gave me a bad input!");
+				System.out.println("I asked for a word.");
+				System.out.print("Try again. ");
+			}
+		}
+		in.close();
+		String newWord = rest + first + pyg;
+		System.out.println("\nThis is your string ("+ word +") in pyglatin: " + newWord);
+	}
+}
