5d17637ff3a84cdad7dcdd3c49e0ccdf44e46a4b
Amith Saligrama <37519861+amithsaligrama@users.noreply.github.com>
2019-03-22 12:24:30 -0400

    Update PygLatin.java

--- PygLatin.java
+++ PygLatin.java
@@ -9,20 +9,11 @@ public class PygLatin {
 		String pyg = "ay";
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter a string to translate into pyglatin: ");
-		while (true) {
-			try {
-				word = in.nextLine();
-				word = word.toLowerCase();
-				first = word.substring(0,1);
-				rest = word.substring(1);
-				break;
-			}
-			catch (Exception e) {
-				System.out.println("You gave me a bad input!");
-				System.out.println("I asked for a word.");
-				System.out.print("Try again. ");
-			}
-		}
+		word = in.nextLine();
+		word = word.toLowerCase();
+		first = word.substring(0,1);
+		rest = word.substring(1);
+		break;
 		in.close();
 		String newWord = rest + first + pyg;
 		System.out.println("\nThis is your string ("+ word +") in pyglatin: " + newWord);
