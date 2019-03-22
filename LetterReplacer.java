6bc66422efc90ab2eff7eefe3ebaa44e84fe23d9
Amith Saligrama <37519861+amithsaligrama@users.noreply.github.com>
2019-03-22 12:28:52 -0400

    Add files via upload

---
+++ LetterReplacer.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LetterReplacer {
+
+	public static void main(String[] args) {
+		String str = "";
+		String n = "a";
+		String m = "b";
+		String replaced = "c";
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a message: ");
+		try {
+			str = in.nextLine();
+			System.out.print("What is a sequence of characters you want to remove from your message (leave blank if you do not want to remove any characters)? ");
+			n = in.nextLine();
+			System.out.print("What do you want to replace that with (leave blank if you do not want to replace that string)? ");
+			m = in.nextLine();
+			replaced = str.replace(n, m);
+		}
+		catch (Exception e) {
+			System.out.println("You gave me a bad value!");
+			System.out.print("Try again. ");
+		}
+		in.close();
+		System.out.println("Here is your encrypted message: " + replaced);
+	}
+
+}
